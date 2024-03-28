19686905d6d10426662904d7a393c41e035f1d6c
taznimt <taznimanwar06@gmail.com>
2024-03-28 15:27:51 +0700

    membuat step11

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -74,6 +74,17 @@ void q_short(int low, int high)
 		}
 		cmp_count++;
 		if (i < j)//step9
+		{
+			// swap the element at index i whit the element at index j
+			swap(i, j);
+		}
+	}
+
+	if (low < j) { //step11
+		//swap the pivot element with the element at index j
+		swap(low, j);
+
+	}
 	
 	
 
