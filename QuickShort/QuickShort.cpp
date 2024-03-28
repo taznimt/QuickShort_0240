8b4b0c49580b20c74567e96b855b7420537cb10a
taznimt <taznimanwar06@gmail.com>
2024-03-28 15:11:10 +0700

    membuat swap

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -34,4 +34,14 @@ void input() {
 	}
 }
 
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
+}
+
 
