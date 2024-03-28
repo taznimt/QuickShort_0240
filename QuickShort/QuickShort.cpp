ab80f52b306b1bbb7aab8cd19b34f7526523a319
taznimt <taznimanwar06@gmail.com>
2024-03-28 15:24:05 +0700

    membuat step7

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -64,6 +64,9 @@ void q_short(int low, int high)
 			i++; //step6
 			cmp_count++;
 		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (i >= low)) //step7
 	
 	
 
