8d83709b62c59b66f4cd50ae85bf40c48494d971
taznimt <taznimanwar06@gmail.com>
2024-03-28 15:09:59 +0700

    membuat input

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -10,3 +10,28 @@ int cmp_count = 0; // number of comparas
 int mov_count = 0; // number of data mov
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+
+	}
+
+	cout << "\n===================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n===================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+
