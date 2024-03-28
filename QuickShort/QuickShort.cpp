4c21579a1130cf3858a7ffad763babf4a3bb2eee
taznimt <taznimanwar06@gmail.com>
2024-03-28 15:23:09 +0700

    membuat step6

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -61,6 +61,9 @@ void q_short(int low, int high)
 		//search for an element greater than pivot
 		while ((arr[i] <= pivot) && (i <= high)) //step5
 		{
+			i++; //step6
+			cmp_count++;
+		}
 	
 	
 
